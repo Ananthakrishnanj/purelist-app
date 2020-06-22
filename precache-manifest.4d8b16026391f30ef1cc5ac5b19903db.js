5f87946169d5ce686de7dd21f12c09e474013880
Alex <alex.oommen@softvision.com>
2020-06-23 00:37:04 +0530

    Updates

--- precache-manifest.4d8b16026391f30ef1cc5ac5b19903db.js
+++ precache-manifest.4d8b16026391f30ef1cc5ac5b19903db.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ac9ecd0fa8fa172e382651e19ad6e2aa",
+    "revision": "3daa4813aca8a08d24aa2e8426646241",
     "url": "/purelist-app/index.html"
   },
   {
-    "revision": "719e55a8f99f26563f5e",
+    "revision": "48640d6ab7bd31ac16c1",
     "url": "/purelist-app/static/css/main.23038552.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/purelist-app/static/js/2.9d8e3321.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "719e55a8f99f26563f5e",
-    "url": "/purelist-app/static/js/main.94036bd3.chunk.js"
+    "revision": "48640d6ab7bd31ac16c1",
+    "url": "/purelist-app/static/js/main.ca518aa3.chunk.js"
   },
   {
     "revision": "1c78274c7708a0f0790d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/purelist-app/precache-manifest.e003cf10d1b1d90d5e60eb5e5f381c68.js"
+  "/purelist-app/precache-manifest.4d8b16026391f30ef1cc5ac5b19903db.js"
 );
 
 self.addEventListener('message', (event) => {
