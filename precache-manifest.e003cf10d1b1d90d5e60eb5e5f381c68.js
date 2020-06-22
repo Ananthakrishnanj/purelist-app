4102fcf1afb2851292442f6bb9474973a37ab1a7
Alex <alex.oommen@softvision.com>
2020-06-23 00:22:17 +0530

    Updates

---
+++ precache-manifest.e003cf10d1b1d90d5e60eb5e5f381c68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac9ecd0fa8fa172e382651e19ad6e2aa",
+    "url": "/purelist-app/index.html"
+  },
+  {
+    "revision": "719e55a8f99f26563f5e",
+    "url": "/purelist-app/static/css/main.23038552.chunk.css"
+  },
+  {
+    "revision": "61e212950ce734f433cf",
+    "url": "/purelist-app/static/js/2.9d8e3321.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/purelist-app/static/js/2.9d8e3321.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "719e55a8f99f26563f5e",
+    "url": "/purelist-app/static/js/main.94036bd3.chunk.js"
+  },
+  {
+    "revision": "1c78274c7708a0f0790d",
+    "url": "/purelist-app/static/js/runtime-main.ad2a591f.js"
+  }
+]);
